5076f9d7d1f48b21c6646b0aaa99d3b7e9cead90
Masrafi <masrafianam@gmail.com>
2023-03-28 13:58:34 +0600

    Initial Commit

---
+++ Blind Date SwiftUI/Blind_Date_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blind_Date_SwiftUIApp.swift
+//  Blind Date SwiftUI
+//
+//  Created by user on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Blind_Date_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Blind Date SwiftUIUITests/Blind_Date_SwiftUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Blind_Date_SwiftUIUITestsLaunchTests.swift
+//  Blind Date SwiftUIUITests
+//
+//  Created by user on 3/28/23.
+//
+
+import XCTest
+
+final class Blind_Date_SwiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
